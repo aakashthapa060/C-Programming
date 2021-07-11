f2d59e9064e56daa3097c4f55975ebca2afc521c
Aakash Thapa <70953207+aakashthapa060@users.noreply.github.com>
2021-07-11 19:03:43 +0545

    Create matrixAddition.c

---
+++ matrixAddition.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	int mat1[2][2],mat2[2][2],mat[2][2],i,j;
+
+	printf("Enter Elements of Matrix 1: \n");
+	for(i = 0; i < 2; i++){
+		for(j = 0; j < 2; j++){
+			printf("Enter Element: ");
+			scanf("%d", &mat1[i][j]);
+		}
+	}
+  
+	printf("\n");
+
+	printf("Enter Elements of Matrix 2: \n");
+	for(i = 0; i < 2; i++){
+		for(j = 0; j < 2; j++){
+			printf("Enter Element: ");
+			scanf("%d", &mat2[i][j]);
+		}
+	}
+
+	for(i = 0; i < 2; i++){
+		for(j = 0; j < 2; j++){
+			mat[i][j] = mat1[i][j] + mat2[i][j];
+		}
+	}
+	
+	for(i = 0; i < 2; i++){
+		for(j = 0; j < 2; j++){
+			printf("%d \t", mat[i][j]);
+		}
+		printf("\n");
+	}
+
+
+	return 0;
+}
